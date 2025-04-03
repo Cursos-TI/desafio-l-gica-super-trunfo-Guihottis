f5b9b056dc279059a0681a4f48a99bc1fe907e82
Guihottis <Guihottis@gmail.com>
2025-04-03 14:59:29 +0000

    Criei o code sobre analisar qual estado tem mais população

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h>  
 
 // Desafio Super Trunfo - Países
 // Tema 2 - Comparação das Cartas
@@ -6,38 +6,38 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
-
+    #include <stdio.h>
+    #include <string.h>
     
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
-
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
-
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
-
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
-
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
+    int main() {
+        char estado1[100], estado2[100];
+        int populacao1, populacao2;
+    
+        printf("insira o nome do primeiro estado: \n");
+        fgets(estado1, sizeof(estado1), stdin);
+        estado1[strcspn(estado1, "\n")] = 0; 
+    
+        printf("insira a populacao do primeiro estado sem pontos ex: 10000: \n");
+        scanf("%d", &populacao1);
+        getchar(); 
+    
+        printf("insira o nome do segundo estado: \n");
+        fgets(estado2, sizeof(estado2), stdin);
+        estado2[strcspn(estado2, "\n")] = 0; 
+    
+        printf("insira a populacao do segundo estado sem pontos ex: 10000: \n");
+        scanf("%d", &populacao2);
+    
+        // Comparação das populações
+        if (populacao1 > populacao2) {
+            printf("o estado %s tem mais população que o estado %s\n", estado1, estado2);
+        } else if (populacao2 > populacao1) {
+            printf("o estado %s tem mais população que o estado %s\n", estado2, estado1);
+        } else {
+            printf("os estados %s e %s tem a mesma população\n", estado1, estado2);
+        }
+    
+        return 0;
 
     return 0;
 }
